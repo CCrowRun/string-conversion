93c15e541683a66cfbbb457fe5b51cc51916b25c
Carrie <carrie.g.crow@gmail.com>
2020-01-14 19:36:25 +0000

    Implemented edge cases for negative and floats

--- string-conversion.rb
+++ string-conversion.rb
@@ -1,24 +1,24 @@
 test_string = "123"
 test_string2 = "4"
+test_string3 = "-9803"
+test_string4 = "14.32"
+
 
 def convert_to_int(string)
-  mapping = {
-    "0" => 0,
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9
-  }
+  #Check for floats
+  string = string.split('.')[0] if string.include?('.')
+  #Check for negative numbers
+  sign = 1
+  if string[0] == '-'
+    string = string[1, string.length-1]
+    sign = -1
+  end
   num = 0
   string.each_char do |char|
-    num = num * 10 + mapping[char]
+    #Use ASCII character code, and subtract 48 (the character code of '0'
+    num = num * 10 + (char.ord - 48)
   end
-  return num
+  return num * sign
 end
 
 convert_to_int(test_string)
